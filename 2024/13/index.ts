1d934955a6aa25b387e1a046af1645d7057f37a1
Ben Fawcett <ben.fawcett@nqry.com.au>
2024-12-13 22:06:31 +1030

    Day 13.

--- 2024/13/index.ts
+++ 2024/13/index.ts
@@ -1,5 +1,6 @@
 import {
   readFile,
+  sumObjArr,
 } from "../../utils/utils";
 import path from "path";
 
@@ -16,6 +17,8 @@ class Machine {
   buttonBy: number;
   prizeX: number;
   prizeY: number;
+  prizeXP2: number;
+  prizeYP2: number;
   constructor(
     buttonAx: number,
     buttonAy: number,
@@ -30,11 +33,18 @@ class Machine {
     this.buttonBy = buttonBy;
     this.prizeX = prizeX;
     this.prizeY = prizeY;
+    this.prizeXP2 = prizeX + 10000000000000;
+    this.prizeYP2 = prizeY + 10000000000000;
   }
 
-  getSolution(): Solution {
-      const timesB = (this.prizeY - ((this.prizeX*this.buttonBy)/this.buttonAx)) / (((this.buttonBx*this.buttonAy)/this.buttonAx) + this.buttonBy);
+  getSolution(): Solution | null {
+
+      const timesB = (this.buttonAx * this.prizeY - this.prizeX* this.buttonAy) / (this.buttonBy * this.buttonAx - this.buttonAy * this.buttonBx);
       const timesA = (this.prizeX - timesB*this.buttonBx ) / this.buttonAx;
+
+      if (!Number.isInteger(timesA) || !Number.isInteger(timesB)) {
+        return null;
+      }
 
       const Acost = 3;
       const Bcost = 1;
@@ -44,24 +54,57 @@ class Machine {
         bPushes: timesB,
         cost: timesA * Acost + timesB * Bcost,
       }
+  }
 
+  getSolutionP2(): Solution | null {
+
+      const timesB = (this.buttonAx * this.prizeYP2 - this.prizeXP2* this.buttonAy) / (this.buttonBy * this.buttonAx - this.buttonAy * this.buttonBx);
+      const timesA = (this.prizeXP2 - timesB*this.buttonBx ) / this.buttonAx;
+
+      if (!Number.isInteger(timesA) || !Number.isInteger(timesB)) {
+        return null;
+      }
+
+      const Acost = 3;
+      const Bcost = 1;
+
+      return {
+        aPushes: timesA,
+        bPushes: timesB,
+        cost: timesA * Acost + timesB * Bcost,
+      }
   }
 
 
 }
 
 const main = async () => {
-  const dataStr = await readFile(path.resolve(__dirname, "./input1.txt"));
+  const dataStr = await readFile(path.resolve(__dirname, "./input.txt"));
   const machines = dataStr.split('\n\n').map(str => [...str.matchAll(/A: X\+(\d+), Y\+(\d+)\n.*B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)/g)]).map(match => {
     const matchArr = match[0];
     if (!matchArr) {
-      return
+      throw 'No matches found!';
     }
     return new Machine(+matchArr[1], +matchArr[2], +matchArr[3], +matchArr[4], +matchArr[5], +matchArr[6]);
   });
 
-  const solution = machines[0]?.getSolution();
-  console.log({solution});
+  const totalCostP1 = sumObjArr(machines, (machine) => {
+    const solution = machine.getSolution();
+    if (solution === null) {
+      return 0;
+    }
+    return solution.cost;
+  });
+
+  const totalCostP2 = sumObjArr(machines, (machine) => {
+    const solution = machine.getSolutionP2();
+    if (solution === null) {
+      return 0;
+    }
+    return solution.cost;
+  });
+
+  console.log({totalCostP1, totalCostP2});
 };
 
 main();
